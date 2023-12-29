e0384b6bd78fb67f89ee45dc25aacf42369e57dc
AZ306043 <147122698+AZ306043@users.noreply.github.com>
2023-12-29 12:00:45 +0800

    Add files via upload

--- 112306043_JS_1229/JS_1229/guessNumber.js
+++ 112306043_JS_1229/JS_1229/guessNumber.js
@@ -1,25 +1,36 @@
-// Global variables
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    checkGuess();
+  }
+});
+// Global variable
 let secretNumber = generateRandomNumber();
 
-// 4. Function to generate a random number between 1 and 10
+// Function to generate a random number between 1 and 10
 function generateRandomNumber() {
-  
+  return Math.floor(Math.random() * 10) + 1;
 }
 
 // Function to check the user's guess
 function checkGuess() {
+  console.log('Function is triggered!'); // Check if this log appears in the console
 
-  // Local variable
+  // Local variables
   let userGuess = document.getElementById("userGuess").value;
   let resultElement = document.getElementById("result");
 
-  // 5. Here should be an if-else statement.
-
-  // If the answer is correct, then output "Congratulations! You guessed the correct number!" with picture2 and music2.
-
-  // If the answer is wrong, then output "Sorry, try again!" with picture1 and music1.
-
-  // P.S. Generate a new random number for the next round after getting the correct answer.
+  // If-else statement to check the guess
+  if (userGuess == secretNumber) {
+    // Correct guess
+    resultElement.innerHTML = "Congratulations! You guessed the correct number!";
+    showImageAndPlaySound("picture2.jpeg", "music2.mp3");
+    // Generate a new random number for the next round
+    secretNumber = generateRandomNumber();
+  } else {
+    // Wrong guess
+    resultElement.innerHTML = "Sorry, try again!";
+    showImageAndPlaySound("picture1.jpeg", "music1.mp3");
+  }
 
   // Clear the input field for the next guess
   document.getElementById("userGuess").value = "";
